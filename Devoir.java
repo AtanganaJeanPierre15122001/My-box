e4c0eb9bb8a1d194238aa67635a5a37cadf8287f
lepierro <lepierro15122001@gmail.com>
2022-10-18 10:02:22 +0200

    choix des differents conditions pour un tesat de personnalité

---
+++ Devoir.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Devoir {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("**********Bienvenue dans notre programme d'execution !!!**********");
+        System.out.println("Veuillez entrer votre nom d'utilisateur svp");
+        String a = sc.nextLine();
+        System.out.println("Bienvenue "+a);
+        System.out.println(" Nous allons evaluer votre personnalité!!!!! ");
+        System.out.println("aimez vous : 1-rester dans votre coin  2-Rester avec vos amis  3-Rester avec votre famille");
+        int b = sc.nextInt();
+        System.out.println("si vous êtes confronté à un accident que feriez vous ? : 1- Appeler les secours  2- essayer de sauver les personnes en face de vous  3- fuir le plus rapidement possible pour ne pas être impliqué dans l'accident :");
+        int c = sc.nextInt();
+        System.out.println("Quel endroit préferiez vous ? : 1- Centre commercial  2- Bibliothèque  3- Parc ");
+        int d = sc.nextInt();
+
+
+
+
+
+
+    }
+}
